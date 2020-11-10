9156328548d22feb61b1597e7c29066b08ad7732
Axel Coézard <axel.coezard@gmail.com>
2020-11-10 14:19:59 +0100

    Ajouts

---
+++ connectVPS.sh
@@ -0,0 +1,12 @@
+# Se connecter au serveur
+
+ssh axelcoezard@51.210.148.5 -p 25565
+
+# Lancer un server serve en fond
+
+(npx serve &)
+
+# Pour le tuer, executer la commande
+ps
+# trouver le PID du processus node et faire:
+kill -9 <PID>
